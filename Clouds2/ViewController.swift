f6d801c22c8d1dec8e22787801938e61e07090c9
Alex Mathers <Malecks@Alexs-MacBook-Pro.local>
2016-09-16 13:18:26 -0400

    Save button will send an image of the cloudView to the default iOS ShareSheet.

--- Clouds2/ViewController.swift
+++ Clouds2/ViewController.swift
@@ -11,16 +11,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var cloudView: CloudView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.refreshButton.layer.cornerRadius = 10.0 //(self.refreshButton.frame.width / 2.0)
+        self.refreshButton.layer.cornerRadius = 10.0
+        self.saveButton.layer.cornerRadius = 10.0
         self.refreshButton.backgroundColor = UIColor.white
     }
     
     @IBAction func refreshView(_ sender: AnyObject) {
         self.cloudView.setNeedsDisplay()
+    }
+    
+    @IBAction func shareCloud(_ sender: AnyObject) {
+        self.createCloudImage()
+    }
+    
+    func createCloudImage() {
+        UIGraphicsBeginImageContext(cloudView.frame.size)
+        self.cloudView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        if let cloudImage = UIGraphicsGetImageFromCurrentImageContext() {
+            displayShareSheet(for: cloudImage)
+        }
+        UIGraphicsEndImageContext()
+    }
+    
+    func displayShareSheet(for image: UIImage) {
+        
+        
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
     }
 }
 
